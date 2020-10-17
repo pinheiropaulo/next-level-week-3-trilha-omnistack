66f30a3fac666601f99fda48e0444de639723b6b
Paulo Pinheiro <pauloprap57@gmail.com>
2020-10-17 20:44:11 -0300

    Mobile Development

---
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import styles from "./styles";
+
+function OrphanagesMap() {
+  return (
+    <View style={styles.container}>
+      <Text>OrphanagesMap</Text>
+    </View>
+  );
+}
+
+export default OrphanagesMap;
